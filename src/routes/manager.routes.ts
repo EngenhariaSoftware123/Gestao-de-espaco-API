aea6f4bdfaae67f4b03a926701f02db7c20926c6
Bruno Santos <83880794+bruno34154@users.noreply.github.com>
2024-06-18 13:59:12 -0300

    Update manager.routes.ts

--- src/routes/manager.routes.ts
+++ src/routes/manager.routes.ts
@@ -16,7 +16,7 @@ class ManagerRoutes {
         return this.router;
     }
     postRoutes(){
-        this.router.get('/find-manager', this.managerControllers.findManager.bind(this.managerControllers));
+        this.router.post('/find-manager', this.managerControllers.findManager.bind(this.managerControllers));
         this.router.post('/create-manager', this.managerControllers.createManager.bind(this.managerControllers));
         return this.router;
     }
